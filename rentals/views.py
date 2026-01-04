8dbfba4dfc4f5f81b57b9f147308c085896587e7
Mohammad Hussein <husseinmohamad651@gmail.com>
2026-01-04 14:16:31 +0200

    merged email-verification

--- rentals/views.py
+++ rentals/views.py
@@ -1,22 +1,6 @@
 # rentals/views.py
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import permissions
-from .models import Rental
-from .serializers import RentalSerializer
-
-class UserRentedCarsView(APIView):
-    permission_classes = [permissions.IsAuthenticated]
-
-    def get(self, request):
-        rentals = Rental.objects.filter(user=request.user, status='active')
-        serializer = RentalSerializer(rentals, many=True)
-        return Response(serializer.data, status=200)
-
-
-
-from rest_framework.views import APIView
-from rest_framework.response import Response
 from rest_framework import permissions, status
 from django.shortcuts import get_object_or_404
 from .models import Rental
